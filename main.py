73b889e3c1c59481725eea9e18e9ba905d196411
joelrealubit <joel.realubit@gmail.com>
2026-03-07 04:38:18 +0800

    added better formatted out, and accepts prompts via cli args

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+# Now we can access `args.user_prompt`
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -9,6 +15,12 @@ if api_key == None:
 
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
-    model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
+    model='gemini-2.5-flash', contents=args.user_prompt)
+
+if response.usage_metadata == None:
+    raise RuntimeError("error getting usage_metadata!")
+print("User prompt: Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+print(f"Response: ")
 print(response.text)
