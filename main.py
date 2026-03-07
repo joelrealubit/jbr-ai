bc55ec2ec18a5e0e93be4bf04e4f6328bd16664b
joelrealubit <joel.realubit@gmail.com>
2026-03-08 02:57:35 +0800

    added roles for messages, and --verbose to args

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import argparse
+from google.genai import types
+
 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 # Now we can access `args.user_prompt`
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -15,12 +19,14 @@ if api_key == None:
 
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
-    model='gemini-2.5-flash', contents=args.user_prompt)
+    model='gemini-2.5-flash', contents=messages)
 
 if response.usage_metadata == None:
     raise RuntimeError("error getting usage_metadata!")
-print("User prompt: Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 print(f"Response: ")
 print(response.text)
