983e5238fa30ced7206f4713060fa3f0c6638680
joelrealubit <joel.realubit@gmail.com>
2026-03-09 00:24:18 +0800

    added function for getting inifo on files w/in allowed guardrails for the agent to work with

---
+++ functions/get_files_info.py
@@ -0,0 +1,34 @@
+import os 
+
+def get_files_info(working_directory, directory="."):
+    
+    #1:
+    working_dir_abs = os.path.abspath(working_directory)
+    # print(f'working_dir_abs = {working_dir_abs}')
+    #print(f'directory = {directory}')
+    #2:
+    target_path = os.path.normpath(os.path.join(working_dir_abs, directory))
+    #print(f'target_path = {target_path}')
+
+    #3: 
+    valid_target_dir = os.path.commonpath([working_dir_abs, target_path]) == working_dir_abs
+    if not valid_target_dir:
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    
+    #4
+    if not os.path.isdir(target_path):
+        return f'Error: "{directory}" is not a directory'
+    
+    dir_contents = os.listdir(os.path.abspath(target_path))
+    ret_str =""
+    if directory==".":
+        ret_str = f'Result for current directory:\n'
+    else:
+        ret_str = f'Result for {directory} directory:\n'
+    for item in dir_contents:
+     #   print(f'item = {item}')   
+        item_path = os.path.normpath(os.path.join(target_path, item))
+        ret_str += f'- {item}: file_size={os.path.getsize(item_path)} bytes, is_dir={os.path.isdir(item_path)}\n'
+    return ret_str
+
+        
