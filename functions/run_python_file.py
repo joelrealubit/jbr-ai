794420bfdbabf80d5ce6f0dab638c596002b71dc
joelrealubit <joel.realubit@gmail.com>
2026-03-11 00:15:55 +0800

    added functionality to run any python file w/in allowed directory

---
+++ functions/run_python_file.py
@@ -0,0 +1,40 @@
+import os
+import subprocess
+
+#1
+def run_python_file(working_directory, file_path, args=None):
+    #2
+    working_dir_abs = os.path.abspath(working_directory)
+    target_path = os.path.normpath(os.path.join(working_dir_abs, file_path))
+    valid_target_dir = os.path.commonpath([working_dir_abs, target_path]) == working_dir_abs
+    if not valid_target_dir:
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    #3
+    if not os.path.isfile(target_path):
+        return f'Error: "{file_path}" does not exist or is not a regular file'
+    
+    #4
+    root, extension = os.path.splitext(file_path)
+    if extension != ".py":
+        return f'Error: "{file_path}" is not a Python file'
+
+    command = ["python", file_path]
+    if args != None:
+        for arg in args:
+            command.extend(arg)
+    try:
+        result = subprocess.run(command,cwd= working_dir_abs, capture_output=True, text=True, check=True, timeout=30000)
+        if result.returncode != 0:
+            return f"Process exited with code {result.returncode}"
+        if result.stdout==None and result.stderr==None:
+            return "No output produced"
+
+        result_str = f'STDOUT: {result.stdout} \n STDERR: {result.stderr} \n' 
+
+        return result_str
+    except subprocess.CalledProcessError as e:
+        return f"Error: executing Python file: {e}"
+
+
+
+        
